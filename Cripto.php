63ce3ca3fd66a43df6e2b35a599721697df1d177
Neto Costa <netocjp@gmail.com>
2022-11-30 11:18:12 -0300

    Melhoria na performance

--- Cripto.php
+++ Cripto.php
@@ -100,7 +100,7 @@ class Cripto
         }
     }
 
-    function consultaApi($moeda = 'bitcoin', $opt = 'symbol')
+    function consultaApi($moeda = 'bitcoin')
     {
         $search = [",", "$"];
         $replace = ["", ""];
@@ -111,10 +111,13 @@ class Cripto
         preg_match_all('/<div class="mr-md-3 tw-pl-2 md:tw-mb-0 tw-text-xl tw-font-bold tw-mb-0">(.*?)<\/div>/s', $content, $name);
         preg_match_all('/<div class="tw-flex tw-text-gray-900 dark:tw-text-white tw-mt-2 tw-items-center">(.*?)src="(.*?)"(.*?)<div/s', $content, $image);
         preg_match_all('/data-target="percent-change.percent" (.*?)>(.*?)<\/span>/s', $content, $perc24h);
-        if ($opt == 'name') return trim(explode("(", strip_tags($name[0][0]))[0]);
-        if ($opt == 'image') return $image[2][0];
-        if ($opt == 'symbol') return explode(")", explode("(", strip_tags($name[0][0]))[1])[0];
-        if ($opt == 'price') return trim(str_replace($search, $replace, strip_tags($current_price[0][0])));
-        if ($opt == 'perc24h') return substr(strip_tags(explode(">", $perc24h[0][0])[1]), 0, -1);
+
+        return [
+            'name' => trim(explode("(", strip_tags($name[0][0]))[0]),
+            'image' => $image[2][0],
+            'symbol' => explode(")", explode("(", strip_tags($name[0][0]))[1])[0],
+            'price' => trim(str_replace($search, $replace, strip_tags($current_price[0][0]))),
+            'perc24h' => substr(strip_tags(explode(">", $perc24h[0][0])[1]), 0, -1)
+        ];
     }
 }
